d266d96ce740877a65519fc64e7ffa0ca285b39e
NirajPudasaini <np2289@nyu.edu>
2021-05-29 09:33:45 +0400

    Blinking in a certain position
    Starting point for part 1, the first

---
+++ M5_stack_code/part1_the_first.ino
@@ -0,0 +1,18 @@
+#include "M5Atom.h"
+
+void setup() 
+{
+  M5.begin(true, false, true);
+  delay(20);
+}
+
+void loop() 
+{
+  M5.dis.drawpix(2,2, 0xffffff);
+  Serial.println(F("Pixel is on"));
+  delay(250);
+  M5.dis.clear();
+  Serial.println(F("Pixel is off"));
+  delay(250);
+  M5.update();
+}
