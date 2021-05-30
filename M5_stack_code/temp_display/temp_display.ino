acf759d31669d4fbc049fb0c3b038ce46827b57d
karljk01 <84964918+karljk01@users.noreply.github.com>
2021-05-30 18:59:06 +0400

    temp display
    Used to display temperature, still need to figure out how to get temperature before we can use this

---
+++ M5_stack_code/temp_display/temp_display.ino
@@ -0,0 +1,39 @@
+#include <M5Stack.h>
+
+// the setup routine runs once when M5Stack starts up
+void setup() {
+  
+  // initialize the M5Stack object
+  M5.begin();
+
+  /*
+    Power chip connected to gpio21, gpio22, I2C device
+    Set battery charging voltage and current
+    If used battery, please call this function in your project
+  */
+  M5.Power.begin();
+  
+  // Lcd display
+  M5.Lcd.fillScreen(WHITE);
+  delay(500);
+  M5.Lcd.fillScreen(RED);
+  delay(500);
+  M5.Lcd.fillScreen(GREEN);
+  delay(500);
+  M5.Lcd.fillScreen(BLUE);
+  delay(500);
+  M5.Lcd.fillScreen(BLACK);
+  delay(500);
+
+  // text print
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(10, 10);
+  M5.Lcd.setTextColor(WHITE);
+  M5.Lcd.setTextSize(1);
+  M5.Lcd.printf("Temperature in celsius or fahrenheit:");
+  M5.Lcd.printf("when codes are joined, -> temp"); //edit this text
+
+}
+void loop(){
+  M5.update();
+}
