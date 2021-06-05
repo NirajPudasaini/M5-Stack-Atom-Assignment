9a42a0df14ec5ae4bb9699ab96ee143190c1fa1b
NirajPudasaini <np2289@nyu.edu>
2021-06-05 13:52:15 +0400

    Cleaning directory for part1_theFirst.
    Uploading codes individually for the final part is left

--- part1_the_first/drat_0/Basic_Completefunctinality_thefirstpart.ino
+++ part1_the_first/drat_0/Basic_Completefunctinality_thefirstpart.ino
@@ -8,6 +8,10 @@
 
 #include "M5Atom.h"
 uint8_t DisBuff[2 + 5 * 5 * 3];
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 void setBuff(uint8_t Rdata, uint8_t Gdata, uint8_t Bdata)
 {
   DisBuff[0] = 0x05;
@@ -20,7 +24,15 @@ void setBuff(uint8_t Rdata, uint8_t Gdata, uint8_t Bdata)
   }
 }
 
+<<<<<<< Updated upstream
 float deacceleratingThreshold = 0.1;
+=======
+float LOW_threshold = 100;
+
+float scaledAccX = 0;
+float scaledAccY = 0;
+float scaledAccZ = 0;
+>>>>>>> Stashed changes
 
 float accX = 0;
 float accY = 0;
@@ -78,7 +90,15 @@ void loop()
     break;
   case 3:
     M5.IMU.getAccelData(&accX, &accY, &accZ);
+<<<<<<< Updated upstream
     if (abs(accX) > deacceleratingThreshold && abs(accY) > deacceleratingThreshold&& abs(accZ) > deacceleratingThreshold)
+=======
+    scaledAccX = accX * 1000;
+    scaledAccY = accY * 1000;
+    scaledAccZ = accZ * 1000;
+
+    if ((abs(scaledAccX) > LOW_threshold) && (abs(scaledAccY) > LOW_threshold))
+>>>>>>> Stashed changes
     {
       setBuff(0xff, 0x00, 0x00);
       M5.dis.displaybuff(DisBuff);
@@ -91,13 +111,27 @@ void loop()
     {
       setBuff(0xff, 0x00, 0x00);
       M5.dis.displaybuff(DisBuff);
+<<<<<<< Updated upstream
       delay(40);
+=======
+
+      delay(40);
+      
+>>>>>>> Stashed changes
     }
     break;
 
   case 4:
     M5.IMU.getAccelData(&accX, &accY, &accZ);
+<<<<<<< Updated upstream
      if ((abs(accX) > deacceleratingThreshold) && (abs(accY) > deacceleratingThreshold))
+=======
+    scaledAccX = accX * 1000;
+    scaledAccY = accY * 1000;
+    scaledAccZ = accZ * 1000;
+
+    if ((abs(scaledAccX) > LOW_threshold) && (abs(scaledAccY) > LOW_threshold))
+>>>>>>> Stashed changes
     {
       setBuff(0x80, 0x80, 0x80);
       M5.dis.displaybuff(DisBuff);
